fef9ee88102ef03e2511ba157c16f28bd2db546a
Joaquin <joaquinteleria@gmail.com>
2023-01-27 12:32:27 +0100

    experimenting with helloworld extension

--- my_project/apps/1-hello-world/js/application.js
+++ my_project/apps/1-hello-world/js/application.js
@@ -2,18 +2,12 @@ import PageJsApplication from "pagejs";
 import { AppContainer } from "pagejs/components";
 
 class Application extends PageJsApplication {
-    screenSaverPage = "";
-    screenSaverTimeout = 255;
-    start() {
-
-    }
-    render() {
-        return (
-            <AppContainer>
-
-            </AppContainer>
-        )
-    }
+  screenSaverPage = "";
+  screenSaverTimeout = 255;
+  start() {}
+  render() {
+    return <AppContainer></AppContainer>;
+  }
 }
 
 let app = new Application(wehub);
