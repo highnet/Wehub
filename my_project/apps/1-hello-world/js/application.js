919c1c81442fa49db9e78e2af4136605fd531484
Joaquin <joaquinteleria@gmail.com>
2023-01-27 15:16:21 +0100

    updated landing page to support status sending functionality

--- my_project/apps/1-hello-world/js/application.js
+++ my_project/apps/1-hello-world/js/application.js
@@ -1,6 +1,8 @@
 import PageJsApplication from "pagejs";
 import { PageContainer } from "pagejs/components";
 
+// https://wissen.wetouch.at/wehub-tutorial-pt-4-extensions/ Catch-All Methods
+
 class Application extends PageJsApplication {
   screenSaverPage = "";
   screenSaverTimeout = 255;
@@ -8,6 +10,10 @@ class Application extends PageJsApplication {
   start() {
     function helloServer() {
       wehub.helloworld.logMessage("Hello Server!");
+    }
+
+    function sendStatus() {
+      wehub.helloworld.sendStatus(document.querySelector('input[name="happyness-status"]:checked').value);
     }
 
     console.log("Started Application");
@@ -20,6 +26,12 @@ class Application extends PageJsApplication {
 
     helloServerButton.addEventListener("click", function () {
       helloServer();
+    });
+
+    let sendStatusButton = document.getElementById("sendStatus");
+
+    sendStatusButton.addEventListener("click", function () {
+      sendStatus();
     });
   }
 
