0187052405a5ee996a7a476370fd865a159e2953
Joaquin <joaquinteleria@gmail.com>
2023-01-27 11:24:14 +0100

    Initial Commit

---
+++ my_project/apps/1-hello-world/js/application.js
@@ -0,0 +1,19 @@
+import PageJsApplication from "pagejs";
+import { AppContainer } from "pagejs/components";
+
+class Application extends PageJsApplication {
+    screenSaverPage = "";
+    screenSaverTimeout = 255;
+    start() {
+
+    }
+    render() {
+        return (
+            <AppContainer>
+
+            </AppContainer>
+        )
+    }
+}
+
+let app = new Application(wehub);
