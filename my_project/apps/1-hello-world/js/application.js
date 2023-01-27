3ab46ea44e187dad3804966ae6d7c1a35e35a109
Joaquin <joaquinteleria@gmail.com>
2023-01-27 14:42:47 +0100

    implemented usage of helloworld extension

--- my_project/apps/1-hello-world/js/application.js
+++ my_project/apps/1-hello-world/js/application.js
@@ -4,12 +4,27 @@ import { PageContainer } from "pagejs/components";
 class Application extends PageJsApplication {
   screenSaverPage = "";
   screenSaverTimeout = 255;
+
   start() {
+    function helloServer() {
+      wehub.helloworld.logMessage("Hello Server!");
+    }
+
     console.log("Started Application");
-    wehub.helloworld.logMessage("Hello World!");
+
+    wehub.addEventListener("helloworld", "hello-client", (...args) => {
+      console.log("Hello Client!", args[0]);
+    });
+
+    let helloServerButton = document.getElementById("helloServer");
+
+    helloServerButton.addEventListener("click", function () {
+      helloServer();
+    });
   }
+
   render() {
-    return <PageContainer></PageContainer>;
+    return <PageContainer />;
   }
 }
 
