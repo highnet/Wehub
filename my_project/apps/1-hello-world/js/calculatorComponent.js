51bc0a1152428e38aa862cfcd4c3aab423488492
Joaquin <joaquintelleria@gmail.com>
2023-01-31 13:47:55 +0100

    -

--- my_project/apps/1-hello-world/js/calculatorComponent.js
+++ my_project/apps/1-hello-world/js/calculatorComponent.js
@@ -6,6 +6,9 @@ export default class CalculatorComponent extends Component {
     MAIN_OPERANDS_BUTTON_GRID_BUTTONS: 12,
     MAIN_OPERANDS_BUTTON_GRIS_COLUMNS: 3,
   };
+
+  debugToggle() {}
+
   render() {
     return (
       <div class="calculator-component">
@@ -29,14 +32,29 @@ export default class CalculatorComponent extends Component {
             }}
           />
         </div>
+        <div>
+          <h2>DEBUG</h2>
+          <fieldset>
+            <h3>Button Labels</h3>
+            <input type="radio" name="debug__button-labels" id="debug__toggle-button-label__identifiers" value="identifiers" checked="checked"></input>
+            <label for="button-identifiers-on">Identifiers</label>
+            <input type="radio" name="debug__button-labels" id="debug__toggle-button-label__calculator-labels" value="calculator-labels"></input>
+            <label for="button-identifiers-off">Calculator Labels</label>
+          </fieldset>
+        </div>
       </div>
     );
   }
+
   ready() {
     for (let i = 0; i < this.calculatorAttributes.MAIN_OPERANDS_BUTTON_GRID_BUTTONS; i++) {
-      console.log(this.getElementById("main-operands-" + i));
+      // console.log(this.getElementById("main-operands-" + i));
     }
 
+    let debugToggleButtonLabelIdentifiers = this.getElementById("debug__toggle-button-label__identifiers");
+    let debugToggleButtonLabelCalculatorLabels = this.getElementById("debug__toggle-button-label__calculator-labels");
+
+    debugToggleButtonLabelIdentifiers.addEventListener("change", this.debugToggle());
     /*
     let btn = this.getElementById("btn-x");
 
