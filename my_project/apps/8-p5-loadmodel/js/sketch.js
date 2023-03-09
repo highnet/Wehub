7a83a90b4b2b060744453b914dbf0c47fa32bca7
joaquin telleria <joaquintelleria@gmail.com>
2023-03-09 23:01:25 +0100

    ,

---
+++ my_project/apps/8-p5-loadmodel/js/sketch.js
@@ -0,0 +1,24 @@
+
+_width = 600;
+_height = 300;
+
+//draw a spinning octahedron
+let teapot;
+
+function preload() {
+  octahedron = loadModel("./octahedron.obj");
+}
+
+function setup() {
+  createCanvas(600, 600, WEBGL);
+}
+
+function draw() {
+  background(200);
+  rotateX(frameCount * 0.01);
+  rotateY(frameCount * 0.01);
+  scale(50);
+  model(teapot);
+
+}
+
